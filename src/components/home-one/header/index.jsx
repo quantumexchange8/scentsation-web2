7de2f4ef382020f5ef764b9213dd89a795002084
jrjingru <163561127+jrjingru@users.noreply.github.com>
2024-12-06 10:32:49 +0800

    fix: navbar

--- src/components/home-one/header/index.jsx
+++ src/components/home-one/header/index.jsx
@@ -1,35 +1,45 @@
-/* eslint-disable for-direction */
 import DesktopNav from "../../common/navigation/desktop-nav/DesktopNav";
-import Dropdown from "../../common/navigation/desktop-nav/Dropdown";
-import DropdownItem from "../../common/navigation/desktop-nav/DropdownItem";
-import NavItem from "../../common/navigation/desktop-nav/NavItem";
-import { menuItemsData } from "../../common/navigation/mobile-nav/menuItemsData";
 import MobileNavbar from "../../common/navigation/mobile-nav/MobileNavbar";
+import { menuItemsData } from "../../common/navigation/mobile-nav/menuItemsData";
 import HeaderButton from "./HeaderButton";
 import HeaderLogo from "./HeaderLogo";
 
-
 function Header() {
+	const currentPath = window.location.pathname; // Get the current path
+
 	return (
 		<header className="site-header aximo-header-section aximo-header1 dark-bg" id="sticky-menu">
 			<div className="container">
 				<nav className="navbar site-navbar">
 					<HeaderLogo />
 					<div className="menu-block-wrapper">
-						<DesktopNav>
-							<NavItem url="/">Home</NavItem>
-							<NavItem url="about-us">About Us</NavItem>
-							<NavItem url="how-to-use">How To Use</NavItem>
-							<NavItem url="partnership">Partnership</NavItem>
-							<NavItem url="contact-us">Contact Us</NavItem>
-							{/* <NavItem dropdown title="Blog">
-								<Dropdown>
-									<DropdownItem url="blog">Our Blog</DropdownItem>
-									<DropdownItem url="blog-grid">Blog Grid</DropdownItem>
-									<DropdownItem url="single-blog">Blog Details</DropdownItem>
-								</Dropdown>
-							</NavItem> */}
-						</DesktopNav>
+						<ul className="desktop-nav">
+							<li>
+								<a href="/" className={currentPath === "/" ? "active" : ""}>
+									Home
+								</a>
+							</li>
+							<li>
+								<a href="/about-us" className={currentPath === "/about-us" ? "active" : ""}>
+									About Us
+								</a>
+							</li>
+							<li>
+								<a href="/how-to-use" className={currentPath === "/how-to-use" ? "active" : ""}>
+									How To Use
+								</a>
+							</li>
+							<li>
+								<a href="/partnership" className={currentPath === "/partnership" ? "active" : ""}>
+									Partnership
+								</a>
+							</li>
+							<li>
+								<a href="/contact-us" className={currentPath === "/contact-us" ? "active" : ""}>
+									Contact Us
+								</a>
+							</li>
+						</ul>
 					</div>
 					<MobileNavbar menuItemsData={menuItemsData} title={<HeaderLogo />} />
 				</nav>
